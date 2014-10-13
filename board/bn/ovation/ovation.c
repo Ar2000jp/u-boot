8ec7226eb7e0e677416efadd78fc831ddb3d1fff
Ahmad Draidi <ar2000jp@gmail.com>
2014-10-13 15:21:33 +0300

    omap4_ovation: Graphics output & framebuffer console support.
    This patch doesn't include full support for the OMAP4 display subsystem,
      instead it makes use of the pre-initialized hardware.

--- board/bn/ovation/ovation.c
+++ board/bn/ovation/ovation.c
@@ -11,6 +11,7 @@
  */
 #include <common.h>
 #include <asm/gpio.h>
+#include <video_fb.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/arch/mmc_host_def.h>
 
@@ -76,4 +77,64 @@ void dram_init_banksize(void)
 	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
 	gd->bd->bi_dram[0].size = 0x40000000;
 	gd->ram_size = 0x40000000;
+}
+
+#define DISPC_CONTROL1_REG_ADDR		0x48041040
+#define DISPC_CONTROL1_GOLCD		(1 << 5)
+
+#define DISPC_GFX_SIZE_REG_ADDR		0x4804108C
+#define DISPC_GFX_SIZE_SIZEX(x)		(((x) - 1) & 0x7FF)
+#define DISPC_GFX_SIZE_SIZEY(y)		((((y) - 1) & 0x7FF) << 16)
+#define DISPC_GFX_SIZE_SIZEX_MASK	(0x7FF)
+#define DISPC_GFX_SIZE_SIZEY_MASK	(0x7FF << 16)
+
+#define DISPC_GFX_POSITION_REG_ADDR	0x48041088
+#define DISPC_GFX_POSITION_POSX(x)	((x) & 0x7FF)
+#define DISPC_GFX_POSITION_POSY(y)	(((y) & 0x7FF) << 16)
+#define DISPC_GFX_POSITION_POSX_MASK	(0x7FF)
+#define DISPC_GFX_POSITION_POSY_MASK	(0x7FF << 16)
+
+#define DISPC_GFX_ATTRIBUTES_REG_ADDR	0x480410A0
+#define DISPC_GFX_ATTRIBUTES_ENABLE	(1)
+#define DISPC_GFX_ATTRIBUTES_FORMAT_XRGB24_8888	((0x08) << 1)
+#define DISPC_GFX_ATTRIBUTES_FORMAT_MASK (0x0F << 1)
+
+GraphicDevice gdev;
+
+void *video_hw_init(void)
+{
+	/* Since the display subsystems are already initialized, we only change what we need. */
+
+	/* Stop display controller */
+	clrbits_le32(DISPC_GFX_ATTRIBUTES_REG_ADDR, DISPC_GFX_ATTRIBUTES_ENABLE);
+
+	/* Set display size */
+	clrbits_le32(DISPC_GFX_SIZE_REG_ADDR, DISPC_GFX_SIZE_SIZEX_MASK);
+	clrbits_le32(DISPC_GFX_SIZE_REG_ADDR, DISPC_GFX_SIZE_SIZEY_MASK);
+	setbits_le32(DISPC_GFX_SIZE_REG_ADDR, DISPC_GFX_SIZE_SIZEX(OVATION_LCD_SIZEX));
+	setbits_le32(DISPC_GFX_SIZE_REG_ADDR, DISPC_GFX_SIZE_SIZEY(OVATION_LCD_SIZEY));
+
+	/* Set window position */
+	clrbits_le32(DISPC_GFX_POSITION_REG_ADDR, DISPC_GFX_POSITION_POSX_MASK);
+	clrbits_le32(DISPC_GFX_POSITION_REG_ADDR, DISPC_GFX_POSITION_POSY_MASK);
+	setbits_le32(DISPC_GFX_POSITION_REG_ADDR, DISPC_GFX_POSITION_POSX(OVATION_LCD_POSX));
+	setbits_le32(DISPC_GFX_POSITION_REG_ADDR, DISPC_GFX_POSITION_POSY(OVATION_LCD_POSY));
+
+	if(OVATION_LCD_GDF == GDF_32BIT_X888RGB){
+	/* Set framebuffer pixel format */
+	clrbits_le32(DISPC_GFX_ATTRIBUTES_REG_ADDR, DISPC_GFX_ATTRIBUTES_FORMAT_MASK);
+	setbits_le32(DISPC_GFX_ATTRIBUTES_REG_ADDR, DISPC_GFX_ATTRIBUTES_FORMAT_XRGB24_8888);
+	}
+
+	/* Validate regs & start display controller */
+	setbits_le32(DISPC_CONTROL1_REG_ADDR, DISPC_CONTROL1_GOLCD);
+	setbits_le32(DISPC_GFX_ATTRIBUTES_REG_ADDR, DISPC_GFX_ATTRIBUTES_ENABLE);
+
+	gdev.frameAdrs = OVATION_LCD_FB_ADDR;
+	gdev.winSizeX = OVATION_LCD_SIZEX;
+	gdev.winSizeY = OVATION_LCD_SIZEY;
+	gdev.gdfBytesPP = OVATION_LCD_BPP;
+	gdev.gdfIndex = OVATION_LCD_GDF;
+
+	return (void *) &gdev;
 }
